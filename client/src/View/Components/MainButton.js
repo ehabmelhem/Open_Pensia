03f98150dab6caf1c01f24460f661373a61ff24e
ibk <ekittani@gmail.com>
2021-02-26 14:55:24 +0200

    main button component

--- client/src/View/Components/MainButton.js
+++ client/src/View/Components/MainButton.js
@@ -1,1 +1,14 @@
-let stam =1
+import React from 'react'
+import './MainButton.css'
+import { Switch, Route, Link, NavLink } from "react-router-dom";
+
+export default function MainButton(props) { // height width tolink text 
+    return (
+        <div>
+            <Link to={props.tolink}>
+            <button className='main-button' style={{width:`${props.width}`,height:`${props.height}`}}>{props.text}</button>
+            </Link>
+
+        </div>
+    )
+}
