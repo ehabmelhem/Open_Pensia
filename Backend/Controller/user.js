26f2fd9faed00f2edb45a8ae93d5aec4df1fc91c
ehab <Melhemehab@gmail.com>
2021-02-26 14:27:34 +0200

    build paths

--- Backend/Controller/user.js
+++ Backend/Controller/user.js
@@ -1,2 +1,5 @@
 const express = require("express");
 const app = express();
+
+exports.addUser=(req,res)=>{
+}
--- Backend/Routers/user.js
+++ Backend/Routers/user.js
@@ -1,3 +1,5 @@
 const router = require("express").Router();
+const userController = require("../Controller/user");
 
+router.post("/add-user", userController.addUser);
 module.exports = router;
