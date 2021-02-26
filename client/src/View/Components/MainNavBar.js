19109502d30c1b1930d4165b88093cd25df5e020
Ali Jbareen <alijb@post.bgu.ac.il>
2021-02-26 15:09:17 +0200

    commit packageJson

--- client/src/View/Components/MainNavBar.js
+++ client/src/View/Components/MainNavBar.js
@@ -1,20 +1,19 @@
-
-import './MainNavBar.css'
+import "./MainNavBar.css";
 
 function MainNavBar({ navTabs }) {
-    return (
-        <nav className='all-tabs'>
-            <ul>
-                {navTabs.map(({className,href,content}, index) => {
-                    return (
-                        <li key={index} className={className}>
-                            <a href={href}>  </a> {content}
-                        </li>
-                    );
-                })}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="all-tabs">
+      <ul>
+        {navTabs.map(({ className, href, content }, index) => {
+          return (
+            <li key={index} className={className}>
+              <a href={href}> </a> {content}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
 export default MainNavBar;
