ea15da48a43ed89d3d77b2bffbdf4ee38ffbb060
ehab <Melhemehab@gmail.com>
2021-03-05 11:23:55 +0200

    add new user middle ware

--- Backend/Controller/user.js
+++ Backend/Controller/user.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-const User = require('../Schema/User');
-
-
+const User = require("../Schema/User");
 
 /* 
 dis: add a new user
@@ -12,10 +10,63 @@ return: {ok:true}
 return: {ok: false,message:'could not add user'}			 
 */
 exports.addUser = async (req, res) => {
-    try{
-  
-}catch(e){
-    console.log('could not run addUser in User')
-}
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      password,
+      fundName,
+      chanel,
+      vote,
+    } = req.body;
+    //middleware
+    console.log("in fun addNewUser");
 
-}
+    //console.log(username, password)
+
+    var datetime = new Date();
+
+    //check if user exists
+    const newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      password: password,
+      status: "Approved", //{Waiting/Approved}
+      fundName: fundName,
+      chanel: chanel,
+      registerDate: datetime,
+      votes: [
+        {
+          proxyCode: vote.proxyCode,
+          officerId: vote.officerId,
+          voted: vote.voted,
+          voteDate: datetime,
+        },
+      ],
+    });
+    const user = await User.findOne({ email: newUser.email });
+
+    if (user !== null) {
+      //    res.send({ ok: false, message: 'user with such user name already exists' })
+      console.log("user with such user name already exists");
+    } else {
+      //   const newUser = new User({ username, password });
+
+      await newUser.save().then(() => {
+        console.log("user saved");
+      });
+      // res.send({ ok: true });
+    }
+
+    res.send({
+      Ok: true,
+    });
+  } catch (e) {
+    console.log("could not run addUser in User");
+    res.send({ Ok: false });
+  }
+};
