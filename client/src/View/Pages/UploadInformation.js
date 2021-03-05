f4688035c1678bbcd2d8cd5deeb27cb647b2985f
Ali Jbareen <alijb@post.bgu.ac.il>
2021-03-05 09:59:00 +0200

    routing MainNavBar working

--- client/src/View/Pages/UploadInformation.js
+++ client/src/View/Pages/UploadInformation.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function UploadInformation() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div>hi</div>;
 }
