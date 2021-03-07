3a2a731c4d343db97d80ba2aaddcc0186f52b078
Mosaab <aborkiamosaab@gmail.com>
2021-03-07 18:33:44 +0200

    Auto stash before merge of "Dashboard_Team" and "origin/Dashboard_Team"

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ import CompanyInfo from "./View/Pages/CompanyInfo";
 import VotesList from "./View/Pages/VotesList";
 import VoteAccept from "./View/Pages/VoteAccept";
 import CardListItem from "./View/Components/CardListItem";
-import Header from "./View/Components/Header";
+import ListQuestions from "./View/Components/body/ListQuestions";
+import Body from "./View/Components/body/Body";
 
 function App() {
   return (
@@ -121,49 +122,12 @@ function App() {
       </nav>
       <Switch>
       <Route path="/TestDashBoardIbraheem">
-        <Header company='בנק הפועלים' perception='18.3'/>
+        {/*<Header company='בנק הפועלים' perception='18.3'/>*/}
         </Route>
         <Route path="/TestDashBoardMosaab">
       
           
-        <CardListItem 
-          companyImgSrc="https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/30/b7/8f/30b78f57-ec42-b14e-5b28-1b52a8d803c8/AppIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-5.png/320x0w.jpg"
-          companyName="בנק הפועלים"
-          companyType="קמעונאות מזון"
-          type="companyBar"
-          />
-           <CardListItem 
-          companyImgSrc="https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/30/b7/8f/30b78f57-ec42-b14e-5b28-1b52a8d803c8/AppIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-5.png/320x0w.jpg"
-          companyName="בנק הפועלים"
-          companyType="קמעונאות מזון"
-          type="companyBar"
-          />
-           <CardListItem 
-          companyImgSrc="https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/30/b7/8f/30b78f57-ec42-b14e-5b28-1b52a8d803c8/AppIcon-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-5.png/320x0w.jpg"
-          companyName="בנק הפועלים"
-          companyType="קמעונאות מזון"
-          type="companyBar"
-          />
-          <CardListItem 
-          questionTitle="מי יהיו הדרקטורים בנק הפועלים"
-          questionStatus="ממתין להצבעתך"
-          type="questionBar"
-          />
-           <CardListItem 
-          questionTitle="מי יהיו הדרקטורים בנק הפועלים"
-          questionStatus="ממתין להצבעתך"
-          type="questionBar"
-          />
-           <CardListItem 
-          questionTitle="מי יהיו הדרקטורים בנק הפועלים"
-          questionStatus="ממתין להצבעתך"
-          type="questionBar"
-          />
-           <CardListItem 
-          questionTitle="מי יהיו הדרקטורים בנק הפועלים"
-          questionStatus="ממתין להצבעתך"
-          type="questionBar"
-          />
+        <Body />
 
         </Route>
         <Route path="/FirstPages_1">
