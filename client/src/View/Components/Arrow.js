a1868911e54f6f4f3d609e04a33382fa7670f7b9
ibk <ekittani@gmail.com>
2021-03-07 20:14:42 +0200

    material ui fixes

--- client/src/View/Components/Arrow.js
+++ client/src/View/Components/Arrow.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Arrow.css'
-// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import { Switch, Route, Link, NavLink } from "react-router-dom";
 
 export default function Arrow(props) { // height width tolink text 
     return (
         <div>
             <Link to={props.arrowToLink}>
-            {/* <ChevronRightIcon  style={{color:`${props.color}`}} id="ChevronRightIcon"/> */}
+            <ChevronRightIcon  style={{color:`${props.color}`}} id="ChevronRightIcon"/>
             </Link>
 
         </div>
