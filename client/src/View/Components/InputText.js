54096b81ca45e3c1d33a92b77038589728b457d2
Rwed <rwedkabha1@gmail.com>
2021-03-14 18:53:28 +0200

    first

--- client/src/View/Components/InputText.js
+++ client/src/View/Components/InputText.js
@@ -1,16 +1,23 @@
 
 import './InputText.css'
+import {useState, useEffect } from "react";
 
-function InputText({ texter }) {
+function InputText({textenglish, texter }) {
+    let [index,setindex]=useState(0);
+
     return (
         <div>
             {console.log(texter)}
             <h3 className="textdisplay">
                 {texter}
+                {localStorage.setItem("firstname",texter)}
             </h3>
             <br></br>
 
-            <input type="text" className="textinput" ></input>
+            <input type="text"  onChange={(e) =>
+      {
+        {localStorage.setItem(textenglish,e.target.value)}
+      }} className="textinput" ></input>
             
         </div>
     );
--- client/src/View/Pages/QuestionsBeforeRegister.js
+++ client/src/View/Pages/QuestionsBeforeRegister.js
@@ -3,18 +3,51 @@ import Inputclass from '../Components/InputText'
 import "./QuestionsBeforeRegister.css";
 
 export default function QuestionsBeforeRegister() {
+    const [user,setuser]=useState({firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        password: String,
+        status: String, //{Waiting/Approved}
+        fundName: String,
+        chanel: String,
+        registerDate: String,
+        votes: [
+          {
+            proxyCode: String,
+            officerId: String,
+            voted: Number,
+            voteDate: String
+          },
+        ],});
+ 
+    const lotery = (e) => {
+        e.preventDefault();
+            fetch("/update-user", {
+              method: "POST",
+              headers: {
+                "Accept": "application/json; odata=verbose",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ isname: allname }),
+            })
+              .then((r) => r.json())
+              .then((data) => {
+               console.log("A")
+              });
+    }
     return (
         <div>
             <h3 className="info">נתוני ההצבעה שלך שמורים במערכת</h3>
             <br></br>
-             < Inputclass texter = {"שם פרטי"} ></Inputclass>
-             < Inputclass texter = {"שם משפחה"} ></Inputclass>
+             < Inputclass textenglish  = {"nameprivate"}  texter = {"שם פרטי"} ></Inputclass>
+             < Inputclass textenglish  = {"family"}  texter = {"שם משפחה"} ></Inputclass>
 
-             < Inputclass texter = {"כתובת מייל"} ></Inputclass>
-             < Inputclass texter = {"מספר טלפון"} ></Inputclass>
+             < Inputclass textenglish  = {"email"}  texter = {"כתובת מייל"} ></Inputclass>
+             < Inputclass textenglish  = {"phone"}  texter = {"מספר טלפון"} ></Inputclass>
 
-
-<button className="but">הלאה</button>
+         
+<button  onClick= {sendinfo} className="but">הלאה</button>
         </div>
     )   
 }
