cf54aa49a5e3e0de4e57131536e821f95db7129b
Semat <s.igbaria@gmail.com>
2021-03-19 14:10:50 +0200

    openQuestion - not ready yet

--- Backend/Controller/Proxy.js
+++ Backend/Controller/Proxy.js
@@ -373,6 +373,82 @@ exports.getExpandedHeader = async (req, res) => {
     console.log("getExpandedHeader fun bug");
   }
 };
+////////////////////////////////////////////////////////
+exports.getOpenQuestionsInFund = async (req, res) => {
+  try {
+    const {userId} = req.body;
+    const user = await User.findOne({ _id: userId });
+  //  console.log("current user:", user);
+  
+    // get all corporates
+    console.log("getAllCorporate");
+   // const { fundname, chanell } = req.body;
+    let url = `https://open-pension-tsofen.herokuapp.com/api/interests?filter[fund_name]=${user.fundName}&filter[Chanel]=${user.chanel}&page=0`;
+    // let url = `https://open-pension-tsofen.herokuapp.com/api/interests`;
+    const encodedURI = encodeURI(url);
+    const allResult = [];
+    let settings = { method: "Get" };
+    await fetch(encodedURI, settings)
+      .then((res) => res.json())
+      .then(async (json) => {
+        if (json.info.pages > 1) {
+          for (let i = 1; i <= json.info.pages; i++) {
+            url = `https://open-pension-tsofen.herokuapp.com/api/interests?filter[fund_name]=${user.fundName}&filter[Chanel]=${user.chanel}&page=${i}`;
+            let encodedd = encodeURI(url);
+            await fetch(encodedd, settings)
+              .then((r) => r.json())
+              .then((data) => {
+                console.log(data);
+                for (var key in data.data) {
+                  if (data.data[key]["A AVE Vote"] > 0.05) {
+                    allResult.push(data.data[key]);
+                  }
+                }
+              });
+          }
+          // "fundname":"הראל", "chanell":"גמל/פנסיה"
+        } else {
+          for (var key in json.data) {
+            if (json.data[key]["A AVE Vote"] > 0.05) {
+              allResult.push(json.data[key]);
+            }
+          }
+        }
+      });
+      // group
+
+      const groupBy = (key) => (allResult) =>
+      allResult.reduce((objectsByKeyValue, obj) => {
+        const value = obj[key];
+        objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+        return objectsByKeyValue;
+      }, {});
+
+    const groupByProxy = groupBy("Security_ID");
+    const groupedProxies = groupByProxy(allResult);
+    console.log(groupedProxies);
+    // for (var currentProxy in groupedProxies) {
+    //   console.log(currentProxy);
+     
+    //   let resultVotes = await Proxy.findOne({
+    //     Proxy_code: currentProxy,
+    //     result: true,
+    //   });
+    //   if (resultVotes !== null) resultCounter++;
+    // }
+
+      res.send({doc:allResult});
+
+
+
+
+
+   //////////////////////////////////////////////////////////////
+  } catch (e) {
+    console.log("getOpenQuestionsInFund fun bug");
+    res.send({ login: false});
+  }
+};
 /////////////////////////////////////////////////////////////////
 setInterval(async () => {
   let url = `http://open-pension-tsofen.herokuapp.com/api/dimProxies`;
