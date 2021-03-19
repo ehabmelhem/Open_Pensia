75e061c7d6ffcf0b6ff5aefa8961fe0dd9ac45bd
ehab <Melhemehab@gmail.com>
2021-03-19 11:35:20 +0200

    Add Cookies Permission

--- Backend/Routers/Proxy.js
+++ Backend/Routers/Proxy.js
@@ -1,31 +1,40 @@
 const router = require("express").Router();
 const proxyController = require("../Controller/Proxy");
+const jwt = require("jwt-simple");
+const secret = "1234";
 
+function checkRole(req, res, next) {
+  let role = req.cookies.role;
+  decRole = jwt.decode(role, secret);
+  if (decRole.role === "user") {
+    next();
+  } else {
+    res.send({ ok: false, messege: "you dont have Premeision" });
+  }
+}
 router
   .route("/get-corporate-default-question-data")
-  .post(proxyController.getDefaultQuestion);
+  .post(checkRole, proxyController.getDefaultQuestion);
 // /proxy/get-expanded-header POST
+//  check
 
 router
   .route("/all-Questions-of-corporate")
   .post(proxyController.getAllQuestions);
+//  check
+
 router
   .route("/get-Question-by-secur-Id")
-  .post(proxyController.getQuestionBySecurId);
+  .post(checkRole, proxyController.getQuestionBySecurId);
+router.route("/get-all-fund").post(proxyController.getAllFundNames); // {FundName:[]} Set
+router.route("/get-all-chanell").post(proxyController.getChanellNames); // request param= {fundname:String0} {chanell:[]} Set
+router.route("/get-all-Corporates").post(proxyController.getAllCorporate); // return {Coeporate:[]}
+//  check
+
+router.route("/get-fund-info").post(checkRole, proxyController.getFundInfo); // return
+//  check
 router
-  .route("/get-all-fund")
-  .post(proxyController.getAllFundNames); // {FundName:[]} Set
-router
-  .route("/get-all-chanell")
-  .post(proxyController.getChanellNames); // request param= {fundname:String0} {chanell:[]} Set
-router
-  .route("/get-all-Corporates")
-  .post(proxyController.getAllCorporate); // return {Coeporate:[]}
-  router
-  .route("/get-fund-info")
-  .post(proxyController.getFundInfo); // return 
-  router
   .route("/get-expanded-header")
-  .post(proxyController.getExpandedHeader); // return
+  .post(checkRole, proxyController.getExpandedHeader); // return
 
 module.exports = router;
