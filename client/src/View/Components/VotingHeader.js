8a0d6b3f62190adbb5a77f4bbce132fb45467d41
Alaa-S-M <alaasaabni@gmail.com>
2021-03-22 13:18:08 +0200

    fixed - User error + the nav bar

--- client/src/View/Components/VotingHeader.js
+++ client/src/View/Components/VotingHeader.js
@@ -1,16 +1,39 @@
 import "./VotingHeader.css";
 import AvatarImg from "./AvatarImg";
 import Arrow from "./Arrow";
+import MainNavBar from "./MainNavBar";
 
-function Header({ directorName, company, link }) {
+function VotingHeader({ directorName, company, backToLink }) {
   return (
     <header className="container1 voting-header">
-      <Arrow arrowToLink={link} color="#B7BCCC" />
+      <Arrow arrowToLink={backToLink} color="#B7BCCC" />
       <div className="company-name">{company}</div>
       <AvatarImg src={"https://www.lego.com/cdn/cs/set/assets/blt0bf03ae97678db52/Batman2_App_Sidekick-Tall1.jpg?fit=crop&format=jpg&quality=80&width=800&height=600&dpr=1"} />
       <div className="candidate-name">{directorName}</div>
+      <MainNavBar navTabs={infoDirectorTabs} />
     </header>
   );
 }
 
-export default Header;
+export default VotingHeader;
+
+let infoDirectorTabs = [
+  {
+    id: 0,
+    className: "non-active-nav",
+    toLink: "/about",
+    content: "סיכום פרטים",
+  },
+  {
+    id: 1,
+    className: "non-active-nav",
+    toLink: "/moreInfo",
+    content: "עוד מידע",
+  },
+  {
+    id: 2,
+    className: "non-active-nav",
+    toLink: "/VotingDetails",
+    content: "נתוני הצבעה",
+  },
+];
--- client/src/View/Pages/InfoDirector.js
+++ client/src/View/Pages/InfoDirector.js
@@ -1,53 +1,45 @@
-import React from "react";
-import MainNavBar from "../Components/MainNavBar";
-import Header from "../Components/VotingHeader";
+import React, { useEffect } from "react";
+import VotingHeader from "../Components/VotingHeader";
 import { useParams } from 'react-router'
 import { fetchOfficerData } from '../../redux'
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Switch,
-} from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { addMessage } from '../../redux/action';
+import DetailsOfVoting from "../Components/DetailsOfVoting";
+import CandidateMoreInfo from "../Components/CandidateMoreInfo";
+import CandidateInfo from "../Components/CandidateInfo";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function InfoDirector() {
 
+  const dispatch = useDispatch();
   const { id } = useParams()
   console.log(id)
-  const dispatch = useDispatch();
-  dispatch(fetchOfficerData(id));
+
+  useEffect(() => {
+    dispatch(fetchOfficerData(id));
+  }, [])
+  let votingPer = [{ disApprovePer: "20%", ApprovePer: "80%" }];
 
   return (
-    <div style={{ maxWidth: "600px", margin: "auto" }}>
-      <Header
-        directorName="עיסאווי פריג'"
-        company="בנק הפועלים"
-        link={"/VoteDirectors"}
-      />
-      <MainNavBar navTabs={infoDirectorTabs} />
-    </div>
+    <Router>
+      <div style={{ maxWidth: "600px", margin: "auto" }}>
+        <VotingHeader
+          directorName="עיסאווי פריג'"
+          company="בנק הפועלים"
+          backToLink={`/VoteDirectors/${id}`}
+        />
+
+        <Switch>
+          <Route path="/moreInfo">
+            <CandidateMoreInfo />
+          </Route>
+          <Route path="/VotingDetails">
+            <DetailsOfVoting voting={votingPer} />
+          </Route>
+          <Route path="/">
+            <CandidateInfo />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-let infoDirectorTabs = [
-  {
-    id: 2,
-    className: "non-active-nav",
-    href: "VotingDetails",
-    content: "נתוני הצבעה",
-  },
-  {
-    id: 1,
-    className: "non-active-nav",
-    href: "moreInfo",
-    content: "עוד מידע",
-  },
-  {
-    id: 0,
-    className: "non-active-nav",
-    href: "about",
-    content: "סיכום פרטים",
-  },
-];
