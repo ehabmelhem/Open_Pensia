55b0bec5743bad84b2ae6c01f1d861f523a6c7dd
Alaa-S-M <alaasaabni@gmail.com>
2021-03-24 00:05:57 +0200

    articles and nav bar design

--- client/src/View/Components/Articles.js
+++ client/src/View/Components/Articles.js
@@ -7,7 +7,7 @@ function Articles({articles}) {
             {articles.map((article, index) => {
                 return (
                     <div key={index} className='article'>
-                        <h4 className='title'> {article.title} </h4>
+                        <h4 className='article-title'> {article.title} </h4>
                         <p className='sub-title'> {article.subTitle} </p>
                         <a href={article.link} target='_blank' className='link'> צפייה בכתבה </a>
                     </div>
