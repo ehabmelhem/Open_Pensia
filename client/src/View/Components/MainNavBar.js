7813c35c2bb5c8adb35c7521821f0b66cf6c2930
Tal <tal.yaron@gmail.com>
2021-03-26 14:04:34 +0300

    retrun to alaaa files 1

--- client/src/View/Components/MainNavBar.js
+++ client/src/View/Components/MainNavBar.js
@@ -1,17 +1,16 @@
 import "./MainNavBar.css";
 import { Link, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import DetailsOfVoting from "../Components/DetailsOfVoting";
-import CandidateMoreInfo from "../Components/CandidateMoreInfo";
-import CandidateInfo from "./CandidateInfo";
-import React, { useState } from "react";
+// import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+// import DetailsOfVoting from "../Components/DetailsOfVoting";
+// import CandidateMoreInfo from "../Components/CandidateMoreInfo";
+// import CandidateInfo from "./CandidateInfo";
+// import React, { useState } from "react";
 
 
 
 function MainNavBar({ navTabs }) {
 
-  const [navTabs1, setnavTabs1] = useState(navTabs);
-  let votingPer = [{ disApprovePer: "20%", ApprovePer: "80%" }];
+  // const [navTabs1, setnavTabs1] = useState(navTabs);
 
   function setNavActive(e) {
     const closestLi = !!e.target && e.target.closest("li");
@@ -24,41 +23,17 @@ function MainNavBar({ navTabs }) {
   }
 
   return (
-    <Router>
-      <div>
-        <nav className="all-tabs">
-          <ul id="ul1">
-            {navTabs1.map(({ id, className, href, content }, index) => {
-              return (
-                <li id="li1" key={index} className={className}>
-                  <Link
-                    to={`/${href}`}
-                    onClick={(id) => {
-                      setNavActive(id);
-                    }}
-                  >
-                    {content}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/moreInfo">
-            <CandidateMoreInfo />
-          </Route>
-          <Route path="/VotingDetails">
-            <DetailsOfVoting voting={votingPer} />
-          </Route>
-
-          <Route path="/">
-            <CandidateInfo />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <nav className="all-tabs">
+      <ul>
+        {navTabs.map(({ id, toLink, className, content }) => {
+          return (
+            <Link key={id} to={toLink} onClick={setNavActive}>
+              <li className={className}>{content}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
 
