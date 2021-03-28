78a1af434845a7c6e7cfb000df71d481bd3cd7b6
ehab <Melhemehab@gmail.com>
2021-03-28 15:05:54 +0300

    Format

--- client/src/View/Pages/Login.js
+++ client/src/View/Pages/Login.js
@@ -1,48 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import './Login.css'
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import { useHistory } from "react-router";
 //user- "email":"new.test@gmail.com","password":"newtest"
-import {fetchUserData} from '../../redux';
-import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserData } from "../../redux";
+import { useDispatch, useSelector } from "react-redux";
 
-function Login(){
-    const dispatch = useDispatch();
-    const [userEmail,setEmail]=useState("")
-    const [userPassword,setPassword]=useState("")
-    let user = useSelector(state => !!state.UserReducer && state.UserReducer);
-    
-     useEffect(() => {
-        if(user.login === true){
-            console.log("worked")
-        }
-     }, [user]);
-    function handleLogin(e) {
-        e.preventDefault();
-        dispatch(fetchUserData({userEmail,userPassword}))
-        props.history.push('/AfterRegistrationVoting');
-        console.log(user)
+function Login() {
+  const dispatch = useDispatch();
+  const [userEmail, setEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+  let user = useSelector((state) => !!state.UserReducer && state.UserReducer);
+
+  useEffect(() => {
+    if (user.login === true) {
+      console.log("worked");
     }
-    return(
-        
-           <form onSubmit={handleLogin} className="loginpage">
-                    <h1>כניסה</h1>
-                    <div>
-                        <label className="title-bold">דוא"ל:</label>
-                        <input type="email" className="input-field" onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}></input>
-                    </div>
-                    <div>
-                        <label>סיסמא:</label>
-                        <input type="password" className="input-field" onChange={(e) => {
-                            setPassword(e.target.value);
-                        }} required></input>
-                    </div>
-                    <button type="submit" className="login">כניסה</button>
-                    {/* <label className="message">{error}</label> */}
-                </form>
+  }, [user]);
+  const history = useHistory();
 
-       
-    )
-
+  function handleLogin(e) {
+    e.preventDefault();
+    dispatch(fetchUserData({ userEmail, userPassword }));
+    history.push("/AfterRegistrationVoting");
+    console.log(user);
+  }
+  return (
+    <form onSubmit={handleLogin} className="loginpage">
+      <h1>כניסה</h1>
+      <div>
+        <label className="title-bold">דוא"ל:</label>
+        <input
+          type="email"
+          className="input-field"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div>
+        <label>סיסמא:</label>
+        <input
+          type="password"
+          className="input-field"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        ></input>
+      </div>
+      <button type="submit" className="login">
+        כניסה
+      </button>
+      {/* <label className="message">{error}</label> */}
+    </form>
+  );
 }
-export default Login
+export default Login;
